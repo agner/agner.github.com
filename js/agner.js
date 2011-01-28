c7f8b8eea622ce6f21da9d6dc9826c3c60ee5add
Anthony Ramine <nox@dev-extend.eu>
2011-01-28 23:25:47 +0100

    Add packages' homepage links and descriptions in index list

--- js/agner.js
+++ js/agner.js
@@ -2,14 +2,31 @@ $(function () {
 
 $.getJSON("http://github.com/api/v2/json/repos/show/agner?callback=?",
     function (data) {
+        var repositories = $
+            .grep(
+                data.repositories,
+                function (repos) { return /^(.+)\.agner$/.test(repos.name); })
+            .sort(
+                function (r1, r2) { return r1.name.localeCompare(r2.name) });
         var ul = $('<ul/>');
-        $.each(data.repositories, function (i, repos) {
+        $.each(repositories, function (i, repos) {
             var m = /^(.+)\.agner$/.exec(repos.name);
-            if (m != null) {
-                ul.append(
-                    $('<li/>').append(
-                        $('<a/>').attr('href', repos.url).text(m[1])));
+            if (m == null) {
+                return;
             }
+            var li = $('<li/>').append(
+                    $('<a/>').attr('href', repos.url).text(m[1]));
+            if (repos.homepage != "") {
+                li
+                    .append(' (')
+                    .append($('<a>homepage</a>').attr('href', repos.homepage))
+                    .append(')');
+            }
+            if (repos.description != "") {
+                var div = $('<div class="description"/>');
+                li.append(div.text(repos.description));
+            }
+            ul.append(li);
         });
         $('#packages + p').replaceWith(ul);
     }
